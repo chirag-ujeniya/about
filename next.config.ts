56d49e2208359867ba1075b945a184bd801c4b93
chirag-ujeniya <ukideashare0021@gmail.com>
2025-06-22 11:30:24 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,28 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/nextn',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
